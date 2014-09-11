6f60d740df2baf7a2e64d20d5b57efd915f81cd2
anhvurz90 <anhvurz90@gmail.com>
2014-09-11 21:14:05 +0700

    Add Regex example: UsernameValidator

---
+++ regex/src/main/java/com/anhvurz90/practice/regex/UsernameValidator.java
@@ -0,0 +1,50 @@
+package com.anhvurz90.practice.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UsernameValidator {
+
+  private Pattern pattern;
+  private Matcher matcher;
+  
+  private static final String USERNAME_PATTERN = "^[a-z0-9_-]{3,15}$";
+  
+  public UsernameValidator() {
+    pattern = Pattern.compile(USERNAME_PATTERN);
+  }
+  
+  /**
+   * Validate username with regular expression
+   * @param username username for validation
+   * @return true valid usename, false invalid username 
+   */
+  public boolean validate(final String username) {
+    matcher = pattern.matcher(username);
+    return matcher.matches();
+  }
+  
+  public static void main(String[] args) {
+    UsernameValidator validator = new UsernameValidator();
+    String s = "mkyong34";
+    System.out.format("%s %s\n", s, validator.validate(s)? "matches" : "not matches");
+    
+    s = "mkyong_2002";
+    System.out.format("%s %s\n", s, validator.validate(s)? "matches" : "not matches");
+
+    s = "mkyong-2002";
+    System.out.format("%s %s\n", s, validator.validate(s)? "matches" : "not matches");
+    
+    s = "mk3-4_yong";
+    System.out.format("%s %s\n", s, validator.validate(s)? "matches" : "not matches");
+    
+    s = "mk";
+    System.out.format("%s %s\n", s, validator.validate(s)? "matches" : "not matches");
+    
+    s = "mk@yong";
+    System.out.format("%s %s\n", s, validator.validate(s)? "matches" : "not matches");
+    
+    s = "mkyong123456789_-";
+    System.out.format("%s %s\n", s, validator.validate(s)? "matches" : "not matches");
+  }
+}
