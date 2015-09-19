77f14bad1f773c4459e7664c65201cdaf0fe92d9
anhvurz90 <vu@einsights.com>
2015-09-19 16:22:12 +0700

    Add BigDecimal example

--- test/src/main/java/com/anhvurz90/practice/misc/TestBigDecimal.java
+++ test/src/main/java/com/anhvurz90/practice/misc/TestBigDecimal.java
@@ -22,9 +22,28 @@ public class TestBigDecimal {
 
         BigDecimal b150 = new BigDecimal(150);
 
-        BigDecimal a2 = a1.divide(b150);
-        BigDecimal b2 = b1.divide(b150);
+        BigDecimal a2 = a1.divide(b150, 20, BigDecimal.ROUND_UP);
+        BigDecimal b2 = b1.divide(b150, 20, BigDecimal.ROUND_UP);
 
-        System.out.println(a2.subtract(b2).toString());
+        System.out.println("BigDecimal: " + a2.subtract(b2).doubleValue());
+
+        System.out.println(getHourMinuteRepresentation(a / 150 - b / 150));
+        System.out.println(getHourMinuteRepresentation((a - b) / 150));
+
+        System.out.println(new BigDecimal(12.3333).setScale(2, BigDecimal.ROUND_HALF_DOWN).doubleValue());
+
     }
+
+    private static double getHourMinuteRepresentation(final double time) {
+        if (time < 0) {
+            return (-1) * getHourMinuteRepresentation(-time);
+        }
+        BigDecimal bigDecimalTime = new BigDecimal(time);
+        BigDecimal hour = new BigDecimal(bigDecimalTime.toBigInteger());
+        BigDecimal bigDecimal60 = new BigDecimal(60);
+        BigDecimal bigDecimal100 = new BigDecimal(100);
+        BigDecimal minute = bigDecimalTime.subtract(hour).multiply(bigDecimal60).divide(bigDecimal100);
+        return hour.add(minute).setScale(2, BigDecimal.ROUND_HALF_DOWN).doubleValue();
+    }
+
 }
