743d45b447a45ac4b9125b1f995088acf179e2c8
anhvurz90 <vu@einsights.com>
2016-07-24 16:00:09 +0700

    Add EventTracker - Simple STS sample

---
+++ spring/EventTracker/src/main/java/com/anhvurz90/WebConfig.java
@@ -0,0 +1,12 @@
+package com.anhvurz90;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.anhvurz90")
+public class WebConfig {
+
+}
